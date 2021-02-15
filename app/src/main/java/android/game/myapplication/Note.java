950d81a91d62a458ebb23f8c8480592fea38a23f
Andrew <andrespin@mail.ru>
2021-02-15 17:07:52 +0300

    Класс данных со структурой заметок

---
+++ app/src/main/java/android/game/myapplication/Note.java
@@ -0,0 +1,15 @@
+package android.game.myapplication;
+
+public class Note {
+
+    private final String name;
+    private final String description;
+    private final String date;
+
+
+    public Note(String name, String description, String date) {
+        this.name = name;
+        this.description = description;
+        this.date = date;
+    }
+}
