cd8651cda73c972a1d976fc237086fbdee7fc31d
Andrew <andrespin@mail.ru>
2021-02-24 12:12:22 +0300

    Созданы: список заметок и карточка для элементов списка

---
+++ app/src/main/java/android/game/myapplication/NotesAdapter.java
@@ -0,0 +1,81 @@
+package android.game.myapplication;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
+
+    private static final String TAG = "NoteAdapter";
+    private final List<Note> notes = new ArrayList<>();
+
+    private final NotesAdapterCallback callback;
+
+    public NotesAdapter(NotesAdapterCallback callback) {
+        this.callback = callback;
+    }
+
+    @NonNull
+    @Override
+    public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notes, parent, false);
+        return new NotesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+        holder.onBind(notes.get(position), position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setItems(List<Note> items) {
+        notes.clear();
+        notes.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    class NotesViewHolder extends RecyclerView.ViewHolder {
+
+        private final MaterialTextView textName;
+        private final MaterialTextView textDescription;
+        private final MaterialTextView textDate;
+
+
+        public NotesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textName = itemView.findViewById(R.id.item_name);
+            textDescription = itemView.findViewById(R.id.item_description);
+            textDate = itemView.findViewById(R.id.item_date);
+        }
+
+        public void onBind(Note model, int position) {
+            Log.d(TAG, String.valueOf(position));
+            textName.setText(model.getName());
+            textDescription.setText(model.getDescription());
+            textDate.setText(model.getDate());
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (getAdapterPosition() != RecyclerView.NO_POSITION) {
+                        callback.onOnItemClicked(getAdapterPosition());
+
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/android/game/myapplication/fragments/NotesListFragment.java
+++ app/src/main/java/android/game/myapplication/fragments/NotesListFragment.java
@@ -3,6 +3,9 @@ package android.game.myapplication.fragments;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.game.myapplication.Note;
+import android.game.myapplication.NotesAdapter;
+import android.game.myapplication.NotesAdapterCallback;
+import android.game.myapplication.NotesSpaceDecorator;
 import android.game.myapplication.R;
 import android.game.myapplication.activities.NoteActivity;
 import android.os.Bundle;
@@ -10,6 +13,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,10 +29,12 @@ import java.util.List;
 import java.util.Locale;
 
 
-public class NotesListFragment extends Fragment {
+public class NotesListFragment extends Fragment implements NotesAdapterCallback {
 
     private List<Note> noteList = new ArrayList<>();
     private boolean isLandscapeOrientation;
+
+    private final NotesAdapter notesAdapter = new NotesAdapter(this);
 
 
     public NotesListFragment() {
@@ -46,19 +54,30 @@ public class NotesListFragment extends Fragment {
         super.onCreate(savedInstanceState);
         isLandscapeOrientation =
                 getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        createNotes();
+    }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notes_list, container, false);
     }
 
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        createNotes();
         initView(view);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        notesAdapter.setItems(noteList);
+    }
 
-    private void initView(View view) {
+
+    private void initView1(View view) {
         int padding = getResources().getDimensionPixelSize(R.dimen.default_margin);
         LinearLayout linearLayout = (LinearLayout) view;
         for (int i = 0; i < noteList.size(); i++) {
@@ -81,10 +100,26 @@ public class NotesListFragment extends Fragment {
         }
     }
 
+    private void initView(View view) {
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerNotes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), RecyclerView.VERTICAL));
+        recyclerView.addItemDecoration(new NotesSpaceDecorator(getResources().getDimensionPixelSize(R.dimen.default_margin)));
+        recyclerView.setAdapter(notesAdapter);
+    }
+
+    @Override
+    public void onOnItemClicked(int position) {
+
+    }
+
     private void createNotes() {
         noteList.add(new Note("Name1", "Desc1", "15.02.2021"));
         noteList.add(new Note("Name2", "Desc2", "13.02.2021"));
         noteList.add(new Note("Name3", "Desc3", "11.02.2021"));
+        noteList.add(new Note("Name4", "Desc4", "15.02.2021"));
+        noteList.add(new Note("Name5", "Desc5", "17.02.2021"));
+        noteList.add(new Note("Name6", "Desc6", "18.02.2021"));
     }
 
     private void checkOrientation(Note note) {
@@ -110,9 +145,4 @@ public class NotesListFragment extends Fragment {
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notes_list, container, false);
-    }
 }
